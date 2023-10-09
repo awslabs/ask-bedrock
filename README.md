f58a5c25344c71b160372baf3f16141c509e9ddb
Ulrich Hinze <uhinze@amazon.de>
2023-10-09 16:20:24 +0200

    Initial

--- README.md
+++ README.md
@@ -1,11 +1,61 @@
-## My Project
+# Ask Amazon Bedrock
 
-TODO: Fill this README out!
+Converse with your favorite [Amazon Bedrock](https://aws.amazon.com/bedrock/) large language model from the command line.
 
-Be sure to:
+<p>
+  <img width="1000" src="README.svg">
+</p>
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Its main added value is that it locally persists AWS account and model configuration to enable quick and easy access.
+
+## Installation
+
+⚠️ Requires Python >= 3.9
+
+⚠️ Requires a working AWS CLI setup configured with a profile that allows Amazon Bedrock access. See [CLI documentation](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html) for details.
+
+```
+pip install ask-bedrock
+```
+
+## Usage
+
+To start a conversation, simply enter the following command:
+
+```
+ask-bedrock converse
+```
+
+Upon first run, you will be led through a configuration flow.
+
+If you’re fully configured, the tool will show you a `>>>` prompt and you can start interacting with the configured model.
+
+Multi-line prompts can be wrapped into `<<< >>>` blocks.
+
+To end your interaction, hit `Ctrl + D`. Note that the conversation will be lost.
+
+## Configuration
+
+Ask Amazon Bedrock stores your user configuration in `$HOME/.config/ask-bedrock/config.yaml`. This file may contain several sets of configuration (contexts). For instance, you can use contexts to switch between different models. Use the `--context` parameter to select the context you'd like to use. The default context is `default`.
+
+If no configuration is found for a selected context, a new one is created. If you want to re-configure an existing config, use 
+
+```
+ask-bedrock configure --context mycontext
+```
+
+### Model parameters
+
+This JSON is passed to Langchain during client setup (as `model_kwargs`). The schema depends on the model that is used. Have a look at the [examples](model_params_examples.md).
+
+If you want to configure multiple lines, model parameters can be wrapped in `<<< >>>`.
+
+## Build from source
+
+```
+pip install -r requirements.txt
+python -m build
+```
 
 ## Security
 
---
+++ build/lib/ask_bedrock/main.py
@@ -0,0 +1,161 @@
+import json
+import os
+from collections.abc import Callable
+
+import boto3
+import click
+import yaml
+from langchain.chains import ConversationChain
+from langchain.llms import Bedrock
+from langchain.memory import ConversationBufferMemory
+
+config_file_path = os.path.join(
+    os.path.expanduser("~"), ".config", "ask-bedrock", "config.yaml"
+)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--context", default="default")
+def converse(context: str):
+    config = get_config(context)
+    if not config:
+        click.echo(
+            f"No configuration found for context {context}. Creating new configuration."
+        )
+        config = create_config(None)
+        put_config(context, config)
+
+    start_conversation(config)
+
+
+@cli.command()
+@click.option("--context", default="default")
+def configure(context: str):
+    existing_config = get_config(context)
+    config = create_config(existing_config)
+    put_config(context, config)
+
+
+def start_conversation(config: dict):
+    llm = model_from_config(config)
+    conversation = ConversationChain(llm=llm, memory=ConversationBufferMemory())
+
+    while True:
+        prompt = multiline_prompt(
+            lambda: click.prompt(click.style(">>>", fg="green")), return_newlines=True
+        )
+
+        response = conversation.predict(input=prompt)
+
+        click.secho(response, fg="yellow")
+
+
+def get_config(context: str) -> dict:
+    if not os.path.exists(config_file_path):
+        return None
+    with open(config_file_path, "r", encoding="utf-8") as f:
+        config = yaml.safe_load(f)
+    if not "contexts" in config:
+        return None
+    if not context in config["contexts"]:
+        return None
+    return config["contexts"][context]
+
+
+def put_config(context: str, new_config: dict):
+    if os.path.exists(config_file_path):
+        with open(config_file_path, "r", encoding="utf-8") as f:
+            current_config_file = yaml.safe_load(f)
+    else:
+        os.makedirs(os.path.dirname(config_file_path), exist_ok=True)
+        current_config_file = {"contexts": {}}
+    new_contexts = current_config_file["contexts"] | {context: new_config}
+    new_config_file = current_config_file | {"contexts": new_contexts}
+
+    with open(config_file_path, "w", encoding="utf-8") as f:
+        click.echo(f"Writing configuration to {config_file_path}.")
+        f.write(yaml.dump(new_config_file))
+
+
+def create_config(existing_config: str) -> dict:
+    region = click.prompt(
+        "🌍 Bedrock region",
+        default=existing_config["region"] if existing_config else None,
+    )
+
+    available_profiles = click.Choice(boto3.session.Session().available_profiles)
+    aws_profile = click.prompt(
+        "👤 AWS profile",
+        type=available_profiles,
+        default=existing_config["aws_profile"] if existing_config else None,
+    )
+
+    bedrock = boto3.Session(profile_name=aws_profile).client("bedrock", region)
+    available_models = click.Choice(
+        [
+            model["modelId"]
+            for model in bedrock.list_foundation_models()["modelSummaries"]
+        ]
+    )
+    model_id = click.prompt(
+        "🚗 Model",
+        type=available_models,
+        default=existing_config["model_id"] if existing_config else None,
+    )
+
+    model_params = multiline_prompt(
+        lambda: click.prompt(
+            "🔠 Model params (JSON)",
+            default=existing_config["model_params"] if existing_config else "{}",
+        ),
+        return_newlines=False,
+    )
+    config = {
+        "region": region,
+        "aws_profile": aws_profile,
+        "model_id": model_id,
+        "model_params": model_params,
+    }
+
+    llm = model_from_config(config)
+    prompt = "Human: You are an assistant used in a CLI tool called 'Ask Bedrock'. The user has just completed their configuration. Write them a nice hello message, including saying that it is from you.\nAssistant:"
+
+    try:
+        click.secho(
+            llm.predict(prompt),
+            fg="yellow",
+        )
+    except Exception as e:
+        click.echo(f"Something went wrong while trying out the model, not saving this.")
+        raise e
+
+    return config
+
+
+def model_from_config(config: dict) -> Bedrock:
+    return Bedrock(
+        credentials_profile_name=config["aws_profile"],
+        model_id=config["model_id"],
+        region_name=config["region"],
+        model_kwargs=json.loads(config["model_params"]),
+    )
+
+
+def multiline_prompt(prompt: Callable[[], str], return_newlines: bool) -> str:
+    response = prompt()
+    if response.startswith("<<<"):
+        response = response[3:]
+        newlines = "\n" if return_newlines else ""
+        while not response.endswith(">>>"):
+            response += newlines + prompt()
+        response = response[:-3]
+    return response
+
+
+if __name__ == "__main__":
+    cli()
